f513af37a079d7b462c49820168944e99a4155ef
gugw <ggw_hsd@hotmail.com>
2022-08-20 20:14:33 +0800

    添加js基础语法2

---
+++ wwwroot/js/my.js
@@ -0,0 +1,47 @@
+alert("我在其他文件中的js")
+//获取随机数
+function getRandom(begin, end) {
+    return Math.floor(Math.random() * (end - begin + 1)) + begin;
+}
+
+//返回秒数
+function countDown(time) {
+    var nowTime = +new Date(); //返回毫秒数，如果没有+，则返回对象。
+    var inputTime = +new Date(time);
+    var times = (inputTime - nowTime) / 1000;
+    var d = parseInt(times / 60 / 60 / 24);
+    d = d < 10 ? '0' + d : d;
+    var h = parseInt(times / 60 / 60 % 24);
+    h = h < 10 ? '0' + h : h;
+    var m = parseInt(times / 60 % 60);
+    m = m < 10 ? '0' + m : m;
+    var s = parseInt(times % 60);
+    s = s < 10 ? '0' + s : s;
+    return d + '天' + h + '时' + m + '分' + s + '秒';
+}
+
+function SetArgument(a) {
+    a = "bbb"
+    return a
+}
+
+function Person(name) {
+    this.name = name;
+}
+
+function SetPerson(person) {
+    person.name = "被替换的名字"
+    return person
+}
+
+function clickAdd(thisObject, tbody) {
+    var newtr = thisObject.parentNode.parentNode.cloneNode(true)
+
+    tbody.appendChild(newtr)
+    newtr.querySelector('.a_add').onclick = function() {
+        clickAdd(this, tbody);
+    }
+    newtr.querySelector('.a_delete').onclick = function() {
+        tbody.removeChild(this.parentNode.parentNode)
+    }
+}
