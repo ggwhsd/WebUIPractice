80a0f26877b1565ace07468726b2888195d2eb06
gugw <ggw_hsd@hotmail.com>
2022-10-01 12:31:48 +0800

    add echart.js

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using webStack.Models;
 
+using System.Collections.Generic;
+
 namespace webStack.Controllers;
 
 [ApiController]
@@ -30,5 +32,19 @@ public class ApiController : ControllerBase
     public QuestionAnswer Get()
     {
         return new QuestionAnswer{ answer="yes",forced=true };
+    }
+    [HttpGet("dataApi")]
+    public Data1 Get2()
+    {
+        List<string> xAxis1 = new List<string>();
+        
+        List<int> data1=new List<int>();
+        for(int i =0;i<100;i++)
+        {
+             xAxis1.Add(i.ToString()+"s"); data1.Add(i%10);
+        }
+       
+       
+        return new Data1{ xAxis=xAxis1, data=data1};
     }
 }
---
+++ Models/Data1.cs
@@ -0,0 +1,12 @@
+namespace webStack.Models;
+using System.Collections;
+public class Data1
+{
+    public List<string> xAxis {
+        get;
+        set;
+        }
+    public List<int> data {get;set;}
+
+    
+}
